d0fea56771fcc0bbde112a00ed5a1030d21dd055
Arthur Gaag <62061397+gaagarthur@users.noreply.github.com>
2026-09-03 10:34:46 +0000

    written report option

--- web/main.py
+++ web/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Form, Query, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from typing import List, Optional
@@ -36,7 +36,8 @@ def run_search(
     direcs: List[str] = Form(default=[]),
     data_origin: str = Form(...),
     group_type: Optional[str] = Form(None),
-    selections: List[str] = Form(None)
+    selections: List[str] = Form(None),
+    DOC_gen: str = Form(...)
 ):
 
     outgoing_direcs = [str(i + 1) for i, d in enumerate(cl.DIRECs) if d in direcs]
@@ -57,13 +58,51 @@ def run_search(
             group_type=group_type
         )
 
-    return templates.TemplateResponse(
-        name = "partials/results.html",
-        request= request,
-        context ={
+    context = {
+            "show_report":False,
             "origin": data_origin,
             "summary": summary,
             "chart_data": chart_data,
+        }
+    print(DOC_gen)
+    if DOC_gen == "sim":
+        clear_direc_list = [item.replace("DIREC", "") for item in direcs]
+
+        context.update({
+            "show_report": True,
+            "direcs_selected": ", ".join(clear_direc_list),
+            "classes_selected": ", ".join(selections or []),
+            "get_date": "31/08/2026",
+            "doc_date": "30/08/2026",
+
+        })
+
+
+    return templates.TemplateResponse(
+        name = "partials/results.html",
+        request= request,
+        context =context,
+    )
+
+@app.get("/report")
+def render_report(
+    request: Request,
+    direcs: List[str] = Query(default=[]),
+    data_origin: str = Query("INEP"),
+    group_type: Optional[str] = Query(None),
+    selections: List[str] = Query(default=[])
+):
+
+    clear_direc_list = [item.replace("DIREC", "") for item in direcs]
+    return templates.TemplateResponse(
+        name="partials/report.html",
+        request=request,
+        context={
+            "direcs_selected": ", ".join(clear_direc_list),
+            "classes_selected": ", ".join(selections),
+            "get_date": "31/08/2026",
+            "doc_date": "30/08/2026",
+            
         },
     )
 
